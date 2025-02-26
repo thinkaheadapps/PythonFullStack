0e0af7deb145c29183a695fe6f76504ec62895b8
Divya Rai <thinkaheadapps@gmail.com>
2025-02-26 20:15:05 +0530

    Mastering The essentials - Module4 - Changing Default end in Print 2

---
+++ Certificates/Mastering the Essentials/Module7/02_whileLoop.py
@@ -0,0 +1,7 @@
+# while condition: exicute , increment
+#initilize condition update value
+
+i = 1
+while i <= 10:
+    print("You are the best")
+    i += 1
---
+++ Certificates/Mastering the Essentials/Module7/04_printNumber.py
@@ -0,0 +1,13 @@
+#printing no from 1-10 using for loop
+
+i = 1
+while i <= 10:
+    print(i)
+    i += 1
+
+#print in one line
+
+j = 0
+while j <= 10:
+    print(j, end=" ")
+    j += 1
