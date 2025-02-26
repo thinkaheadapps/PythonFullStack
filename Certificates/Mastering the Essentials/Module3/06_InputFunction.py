f0f9fc191a2b885049eda738eafef0725b2c8d91
Divya Rai <thinkaheadapps@gmail.com>
2025-02-26 18:01:52 +0530

    Mastering The essentials - Input Function

---
+++ Certificates/Mastering the Essentials/Module3/06_InputFunction.py
@@ -0,0 +1,22 @@
+# Input Function
+name = input()
+print(name)
+print(type(name)) #<class 'str'>
+
+#by default type of input is string
+x = input()
+print(x)
+print(type(x))
+
+# Typecasting- type conversion
+
+x = 1.5
+y = float(x)
+print(x)
+print(type(x))
+print(type(y))
+
+
+
+
+
