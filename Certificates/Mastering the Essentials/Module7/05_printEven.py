02cc39310ed9ca245c828e7338a1e59d66428db2
Divya Rai <thinkaheadapps@gmail.com>
2025-02-27 01:05:25 +0530

    Mastering The essentials - Module7 05:even n odd

---
+++ Certificates/Mastering the Essentials/Module7/05_printEven.py
@@ -0,0 +1,13 @@
+# Print even no
+i = 0
+while i <= 10:
+    if i % 2 == 0:
+        print(i, end=" ")
+    i += 1
+
+# Print odd numbers
+j = 0
+while j <= 10:
+    if j % 2 == 1:
+        print(j, end=" ")
+    j += 1
