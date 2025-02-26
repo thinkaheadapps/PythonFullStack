b64a91247c477e8e4bd82beab2c1933161df731a
Divya Rai <thinkaheadapps@gmail.com>
2025-02-26 16:56:29 +0530

    Mastering The essentials - Heap and stack memory and id function

---
+++ Certificates/Mastering the Essentials/Module3/04_IDFunction.py
@@ -0,0 +1,18 @@
+x = 5
+print(x)
+
+# x will be stored in stack memory and value will be stored in Heap memory.
+# To get the address of the stored memory we use ID val;ue
+
+print(id(x))
+
+x = 45
+print(id(x))
+print(x)
+
+#
+name = "Divya"
+print(id(name))
+
+name = "Some Name"
+print(id(name))
