2ecfec8cae55e7ba99373dec119af30b49678798
Divya Rai <thinkaheadapps@gmail.com>
2025-02-26 17:49:03 +0530

    Mastering The essentials - Data Type

---
+++ Certificates/Mastering the Essentials/Module3/05_DataTypes.py
@@ -0,0 +1,41 @@
+#Integers
+#Floats
+#Booleans
+#Strings
+#None
+
+number = 24
+print(number)
+print(type(number)) #<class 'int'>
+
+s = "a"
+print(type(s)) #<class 'str'>
+
+f1 = 10.5
+print(type(f1)) #<class 'float'>
+
+b1 = True
+print(type(b1)) #<class 'bool'>
+
+str1 = "This is sample string"
+print(str1)
+print(type(str1)) #<class 'str'>
+
+str2 = 'with Single quote'
+print(type(str2))
+
+str3 = """This is multiline String!
+This is second Line
+"""
+print(str3) #<class 'str'>
+print(type(str3))
+
+#Null
+
+n = None
+print(n) #None
+print(type(n)) #<class 'NoneType'>
+
+
+
+
