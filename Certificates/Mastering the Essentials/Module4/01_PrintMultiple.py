0cdacb6b275a4ed0f4e0b612c16013b60b836a64
Divya Rai <thinkaheadapps@gmail.com>
2025-02-26 18:52:43 +0530

    Mastering The essentials - Module4 - Changing Default end in Print

--- Certificates/Mastering the Essentials/Module4/01_PrintMultiple.py
+++ Certificates/Mastering the Essentials/Module4/01_PrintMultiple.py
@@ -1,3 +1,4 @@
 print("Divya")
 print("Divya 1", "Divya 2", "Divya 3", 1, 2, 3)
 
+
---
+++ Certificates/Mastering the Essentials/Module4/02_ChengingDefaultSeperator.py
@@ -0,0 +1,10 @@
+print("Divya", 10)
+print("Divya2", 20)
+
+print("abc", 10, end=" ")
+print("def", 20)
+
+print("Rahul", 40, end="->")
+print("Rohit", 30)
+
+
